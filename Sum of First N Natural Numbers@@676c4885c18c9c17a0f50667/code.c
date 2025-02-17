574dd4088c869c8661348dc10c7976f71a457f25
Bhumi <bhumigoyal70@gmail.com>
2025-02-18 01:21:49 +0530

    Solution to Sum of First N Natural Numbers

--- Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
+++ Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
@@ -2,12 +2,10 @@
 #include<stdio.h>
 
 int main() {
-    int N;
+    int N, sum;
     scanf("%d", &N);
     for(int i = 1; i<= N; i++) {
-        for(sum = 0) {
-            sum += 1;
-        }
+        sum += 1
     }
     printf("%d \n", N , sum);
 
